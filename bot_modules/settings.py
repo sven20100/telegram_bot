9a6046837892d0501151615b07946e566e9986c3
sven20100 <makarov.megion086@mail.ru>
2025-05-12 22:09:20 +0500

    Updated settings.py: fixed ADMIN_IDS to include correct ID

--- bot_modules/settings.py
+++ bot_modules/settings.py
@@ -1,8 +1,8 @@
 # Telegram bot token from @BotFather
-TELEGRAM_TOKEN = "7938456191:AAFqpqVGWaIIy9XeOxprp_igDxGYikValBk"  # Замени на токен от @BotFather
+TELEGRAM_TOKEN = "7938456191:AAEHHfz6u-PAm045AatKRCSbFHYc8d_mzrw"  # Замени на токен от @BotFather
 
 # List of admin Telegram IDs
-ADMIN_IDS = [7938456191]  # Твой ID, добавь другие, если нужно
+ADMIN_IDS = [6503798414]  # Твой ID, добавь другие, если нужно
 
 # Dzen profile URLs to parse
 DZEN_PROFILE_URLS = ["https://dzen.ru/id/680e334acbb89444e26a3bd2"]  # Замени на реальные URL-ы Дзена
